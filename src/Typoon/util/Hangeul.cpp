3d1ed6bd43b79778112dc273d6660bc61f155585
yeshjho <yeshjho2@gmail.com>
2025-11-21 23:18:32 +0800

    한글 유틸 함수 추가

--- src/Typoon/util/Hangeul.cpp
+++ src/Typoon/util/Hangeul.cpp
@@ -1,5 +1,7 @@
 ﻿#include "Hangeul.h"
 
+#include "ime/Composition.h"
+
 
 namespace typoon::util
 {
@@ -19,9 +21,124 @@ bool is_hangeul_alphabet(const wchar_t ch)
     return is_hangeul_consonant(ch) || is_hangeul_vowel(ch);
 }
 
+bool is_hangeul_composite(const wchar_t ch)
+{
+    return L'가' <= ch && ch <= L'힣';
+}
+
 bool is_hangeul(const wchar_t ch)
 {
-    return (L'가' <= ch && ch <= L'힣') || is_hangeul_alphabet(ch);
+    return is_hangeul_composite(ch) || is_hangeul_alphabet(ch);
+}
+
+std::wstring decompose_hangeul(const std::wstring_view str)
+{
+    constexpr int lettersOfAnInitial = std::size(DECOMPOSED_MEDIAL_MAP) * std::size(DECOMPOSED_FINAL_MAP);
+
+    std::wstring result;
+    result.reserve(str.size() * 3);
+
+    for (const wchar_t c : str)
+    {
+        if (is_hangeul_composite(c))
+        {
+            const auto [initialIndex, nonInitialPart] = std::div(c - L'가', lettersOfAnInitial);
+            const auto [medialIndex, finalIndex] = std::div(nonInitialPart, std::size(DECOMPOSED_FINAL_MAP));
+            result += INITIAL_MAP[initialIndex];
+            result += DECOMPOSED_MEDIAL_MAP[medialIndex];
+            result += DECOMPOSED_FINAL_MAP[finalIndex];
+        }
+        else if (is_hangeul_consonant(c))
+        {
+            result += DECOMPOSED_CONSONANT_MAP[c - L'ㄱ'];
+        }
+        else if (is_hangeul_vowel(c))
+        {
+            result += DECOMPOSED_MEDIAL_MAP[c - L'ㅏ'];
+        }
+        else
+        {
+            result += c;
+        }
+    }
+
+    return result;
+}
+
+std::wstring combine_hangeul(const std::wstring_view str)
+{
+    std::wstring result;
+    result.reserve(str.size() / 2);
+    core::Composition composition{ [&result](const wchar_t letter) { result += letter; } };
+
+    for (const wchar_t c : str)
+    {
+        composition.AddAlphabet(c);
+    }
+    if (const wchar_t last = composition.ComposeLetter();
+        last != 0)
+    {
+        result += last;
+    }
+
+    return result;
+}
+
+std::wstring latin_alphabet_to_hangeul_alphabet(const std::wstring_view str)
+{
+    std::wstring result;
+    result.reserve(str.size());
+
+    for (const wchar_t c : str)
+    {
+        if ('a' <= c && c <= 'z')
+        {
+            result += LOWER_LATIN_ALPHABET_TO_HANGEUL[c - L'a'];
+        }
+        else if ('A' <= c && c <= 'Z')
+        {
+            result += UPPER_LATIN_ALPHABET_TO_HANGEUL[c - L'A'];
+        }
+        else
+        {
+            result += c;
+        }
+    }
+
+    return result;
+}
+
+std::wstring hangeul_alphabet_to_latin_alphabet(const std::wstring_view str)
+{
+    std::wstring result;
+    result.reserve(str.size());
+
+    // ㄱ~ㅣ
+    constexpr char hangeulAlphabetToLatinAlphabet[] = {
+        'r', 'R', 0, 's', 0, 0, 'e', 'E', 'f', 0, 0, 0, 0, 0, 0, 0, 'a', 'q', 'Q', 0, 't', 'T', 'd', 'w', 'W', 'c', 'z', 'x', 'v', 'g',
+        'k', 'o', 'i', 'O', 'j', 'p', 'u', 'P', 'h', 0, 0, 0, 'y', 'n', 0, 0, 0, 'b', 'm', 0, 'l'
+    };
+
+    for (const wchar_t c : str)
+    {
+        if (is_hangeul_alphabet(c))
+        {
+            if (const char converted = hangeulAlphabetToLatinAlphabet[c - L'ㄱ'])
+            {
+                result += converted;
+            }
+            else
+            {
+                result += c;
+            }
+        }
+        else
+        {
+            result += c;
+        }
+    }
+
+    return result;
 }
 
 }
--- src/UnitTests/tests/util/Hangeul.cpp
+++ src/UnitTests/tests/util/Hangeul.cpp
@@ -2,10 +2,126 @@
 
 #include "Typoon/util/Hangeul.h"
 
+#include "util/DocTestWStringSupport.h"
 
-TEST_SUITE("Util")
+
+using namespace typoon::util;
+
+TEST_SUITE("Hangeul")
 {
-    TEST_CASE("Hangeul")
+    TEST_CASE("decompose_hangeul")
     {
+        SUBCASE("한글이 아닌 문자")
+        {
+            CHECK(decompose_hangeul(L"abcXYZ012,./*() \n\t") == L"abcXYZ012,./*() \n\t");
+            CHECK(decompose_hangeul(L"ℾↁ⯶⼉⤵⿺ⰼ⢳⛚⡸♦⋀⁓ⷵ⌞ⓐ⃣ⴕ⻠⠉⻔⍆ⵦ⍳⏚⻭⟳╊≳⛠⓴◦⿍⭒☄↙⬣⟩ⵘ⁔Ⲏ‍╄ⓨ➗ ⅽ╷⹛✮") == L"ℾↁ⯶⼉⤵⿺ⰼ⢳⛚⡸♦⋀⁓ⷵ⌞ⓐ⃣ⴕ⻠⠉⻔⍆ⵦ⍳⏚⻭⟳╊≳⛠⓴◦⿍⭒☄↙⬣⟩ⵘ⁔Ⲏ‍╄ⓨ➗ ⅽ╷⹛✮");
+        }
+
+        SUBCASE("한글 자모")
+        {
+            CHECK(decompose_hangeul(L"ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ") == L"ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ");
+            CHECK(decompose_hangeul(L"ㅏㅑㅓㅕㅗㅛㅜㅠㅡㅣㅐㅔ") == L"ㅏㅑㅓㅕㅗㅛㅜㅠㅡㅣㅐㅔ");
+            CHECK(decompose_hangeul(L"ㄲㄸㅃㅆㅉㅒㅖ") == L"ㄲㄸㅃㅆㅉㅒㅖ");
+        }
+
+        SUBCASE("한글 한 글자 모음")
+        {
+            CHECK(decompose_hangeul(L"가냐더려모뵤수유즈치캐테파햐") == L"ㄱㅏㄴㅑㄷㅓㄹㅕㅁㅗㅂㅛㅅㅜㅇㅠㅈㅡㅊㅣㅋㅐㅌㅔㅍㅏㅎㅑ");
+            CHECK(decompose_hangeul(L"꺠뗴빠쌰쩌걔녜") == L"ㄲㅒㄸㅖㅃㅏㅆㅑㅉㅓㄱㅒㄴㅖ");
+        }
+
+        SUBCASE("한글 두 글자 모음")
+        {
+            CHECK(decompose_hangeul(L"ㅘㅙㅚㅝㅞㅟㅢ") == L"ㅗㅏㅗㅐㅗㅣㅜㅓㅜㅔㅜㅣㅡㅣ");
+            CHECK(decompose_hangeul(L"꽈뙈뾔쒀쮀귀늬") == L"ㄲㅗㅏㄸㅗㅐㅃㅗㅣㅆㅜㅓㅉㅜㅔㄱㅜㅣㄴㅡㅣ");
+        }
+
+        SUBCASE("한글 한 글자 받침")
+        {
+            CHECK(decompose_hangeul(L"각꺆놘뙏럘뼴법셧옸죵춪큧틐핕힆쒷") == L"ㄱㅏㄱㄲㅑㄲㄴㅗㅏㄴㄸㅗㅐㄷㄹㅒㄹㅃㅖㅁㅂㅓㅂㅅㅕㅅㅇㅗㅆㅈㅛㅇㅊㅜㅈㅋㅠㅊㅌㅡㅋㅍㅣㅌㅎㅡㅣㅍㅆㅜㅔㅎ");
+        }
+
+        SUBCASE("한글 겹받침")
+        {
+            CHECK(decompose_hangeul(L"ㄳㄵㄶㄺㄻㄼㄽㄾㄿㅀㅄ") == L"ㄱㅅㄴㅈㄴㅎㄹㄱㄹㅁㄹㅂㄹㅅㄹㅌㄹㅍㄹㅎㅂㅅ");
+            CHECK(decompose_hangeul(L"갃꺉놚뙑럚뼯벐셡옲죯춦") == L"ㄱㅏㄱㅅㄲㅑㄴㅈㄴㅗㅏㄴㅎㄸㅗㅐㄹㄱㄹㅒㄹㅁㅃㅖㄹㅂㅂㅓㄹㅅㅅㅕㄹㅌㅇㅗㄹㅍㅈㅛㄹㅎㅊㅜㅂㅅ");
+        }
+    }
+
+    TEST_CASE("combine_hangeul")
+    {
+        SUBCASE("한글이 아닌 문자")
+        {
+            CHECK(combine_hangeul(L"abcXYZ012,./*() \n\t") == L"abcXYZ012,./*() \n\t");
+            CHECK(combine_hangeul(L"ℾↁ⯶⼉⤵⿺ⰼ⢳⛚⡸♦⋀⁓ⷵ⌞ⓐ⃣ⴕ⻠⠉⻔⍆ⵦ⍳⏚⻭⟳╊≳⛠⓴◦⿍⭒☄↙⬣⟩ⵘ⁔Ⲏ‍╄ⓨ➗ ⅽ╷⹛✮") == L"ℾↁ⯶⼉⤵⿺ⰼ⢳⛚⡸♦⋀⁓ⷵ⌞ⓐ⃣ⴕ⻠⠉⻔⍆ⵦ⍳⏚⻭⟳╊≳⛠⓴◦⿍⭒☄↙⬣⟩ⵘ⁔Ⲏ‍╄ⓨ➗ ⅽ╷⹛✮");
+        }
+
+        SUBCASE("한글 자모")
+        {
+            CHECK(combine_hangeul(L"ㅎㅍㅌㅋㅊㅈㅇㅅㅂㅁㄹㄷㄴㄱ") == L"ㅎㅍㅌㅋㅊㅈㅇㅅㅂㅁㄹㄷㄴㄱ");
+            CHECK(combine_hangeul(L"ㅔㅐㅣㅡㅠㅜㅛㅗㅕㅓㅑㅏ") == L"ㅔㅐㅣㅡㅠㅜㅛㅗㅕㅓㅑㅏ");
+            CHECK(combine_hangeul(L"ㅒㅖㄲㄸㅃㅆㅉ") == L"ㅒㅖㄲㄸㅃㅆㅉ");
+        }
+
+        SUBCASE("한글 한 글자 모음")
+        {
+            CHECK(combine_hangeul(L"ㄱㅏㄴㅑㄷㅓㄹㅕㅁㅗㅂㅛㅅㅜㅇㅠㅈㅡㅊㅣㅋㅐㅌㅔㅍㅏㅎㅑ") == L"가냐더려모뵤수유즈치캐테파햐");
+            CHECK(combine_hangeul(L"ㄲㅒㄸㅖㅃㅏㅆㅑㅉㅓㄱㅒㄴㅖ") == L"꺠뗴빠쌰쩌걔녜");
+        }
+
+        SUBCASE("한글 두 글자 모음")
+        {
+            CHECK(combine_hangeul(L"ㅗㅏㅗㅐㅗㅣㅜㅓㅜㅔㅜㅣㅡㅣ") == L"ㅘㅙㅚㅝㅞㅟㅢ");
+            CHECK(combine_hangeul(L"ㄲㅗㅏㄸㅗㅐㅃㅗㅣㅆㅜㅓㅉㅜㅔㄱㅜㅣㄴㅡㅣ") == L"꽈뙈뾔쒀쮀귀늬");
+        }
+
+        SUBCASE("한글 한 글자 받침")
+        {
+            CHECK(combine_hangeul(L"ㄱㅏㄱㄲㅑㄲㄴㅗㅏㄴㄸㅗㅐㄷㄹㅒㄹㅃㅖㅁㅂㅓㅂㅅㅕㅅㅇㅗㅆㅈㅛㅇㅊㅜㅈㅋㅠㅊㅌㅡㅋㅍㅣㅌㅎㅡㅣㅍㅆㅜㅔㅎ") == L"각꺆놘뙏럘뼴법셧옸죵춪큧틐핕힆쒷");
+        }
+
+        SUBCASE("한글 겹받침")
+        {
+            CHECK(combine_hangeul(L"ㄱㅅㄴㅈㄴㅎㄹㄱㄹㅁㄹㅂㄹㅅㄹㅌㄹㅍㄹㅎㅂㅅ") == L"ㄳㄵㄶㄺㄻㄼㄽㄾㄿㅀㅄ");
+            CHECK(combine_hangeul(L"ㄱㅏㄱㅅㄲㅑㄴㅈㄴㅗㅏㄴㅎㄸㅗㅐㄹㄱㄹㅒㄹㅁㅃㅖㄹㅂㅂㅓㄹㅅㅅㅕㄹㅌㅇㅗㄹㅍㅈㅛㄹㅎㅊㅜㅂㅅ") == L"갃꺉놚뙑럚뼯벐셡옲죯춦");
+        }
+    }
+
+    TEST_CASE("latin_alphabet_to_hangeul_alphabet")
+    {
+        SUBCASE("라틴 알파벳이 아닌 문자")
+        {
+            CHECK(latin_alphabet_to_hangeul_alphabet(L"ㄱㄴㄷㅏㅒㄲ012,./*() \n\t") == L"ㄱㄴㄷㅏㅒㄲ012,./*() \n\t");
+            CHECK(latin_alphabet_to_hangeul_alphabet(L"áÁǒø") == L"áÁǒø");
+            CHECK(latin_alphabet_to_hangeul_alphabet(L"ℾↁ⯶⼉⤵⿺ⰼ⢳⛚⡸♦⋀⁓ⷵ⌞ⓐ⃣ⴕ⻠⠉⻔⍆ⵦ⍳⏚⻭⟳╊≳⛠⓴◦⿍⭒☄↙⬣⟩ⵘ⁔Ⲏ‍╄ⓨ➗ ⅽ╷⹛✮") == L"ℾↁ⯶⼉⤵⿺ⰼ⢳⛚⡸♦⋀⁓ⷵ⌞ⓐ⃣ⴕ⻠⠉⻔⍆ⵦ⍳⏚⻭⟳╊≳⛠⓴◦⿍⭒☄↙⬣⟩ⵘ⁔Ⲏ‍╄ⓨ➗ ⅽ╷⹛✮");
+        }
+
+        SUBCASE("라틴 알파벳")
+        {
+            CHECK(latin_alphabet_to_hangeul_alphabet(L"abcdefghijklmnopqrstuvwxyz") == L"ㅁㅠㅊㅇㄷㄹㅎㅗㅑㅓㅏㅣㅡㅜㅐㅔㅂㄱㄴㅅㅕㅍㅈㅌㅛㅋ");
+            CHECK(latin_alphabet_to_hangeul_alphabet(L"ABCDEFGHIJKLMNOPQRSTUVWXYZ") == L"ㅁㅠㅊㅇㄸㄹㅎㅗㅑㅓㅏㅣㅡㅜㅒㅖㅃㄲㄴㅆㅕㅍㅉㅌㅛㅋ");
+        }
+    }
+
+    TEST_CASE("hangeul_alphabet_to_latin_alphabet")
+    {
+        SUBCASE("한글이 아닌 문자")
+        {
+            CHECK(hangeul_alphabet_to_latin_alphabet(L"abcXYZ012,./*() \n\t") == L"abcXYZ012,./*() \n\t");
+            CHECK(hangeul_alphabet_to_latin_alphabet(L"áÁǒø") == L"áÁǒø");
+            CHECK(hangeul_alphabet_to_latin_alphabet(L"ℾↁ⯶⼉⤵⿺ⰼ⢳⛚⡸♦⋀⁓ⷵ⌞ⓐ⃣ⴕ⻠⠉⻔⍆ⵦ⍳⏚⻭⟳╊≳⛠⓴◦⿍⭒☄↙⬣⟩ⵘ⁔Ⲏ‍╄ⓨ➗ ⅽ╷⹛✮") == L"ℾↁ⯶⼉⤵⿺ⰼ⢳⛚⡸♦⋀⁓ⷵ⌞ⓐ⃣ⴕ⻠⠉⻔⍆ⵦ⍳⏚⻭⟳╊≳⛠⓴◦⿍⭒☄↙⬣⟩ⵘ⁔Ⲏ‍╄ⓨ➗ ⅽ╷⹛✮");
+        }
+
+        SUBCASE("한글 자모")
+        {
+            CHECK(hangeul_alphabet_to_latin_alphabet(L"ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㅏㅑㅓㅕㅗㅛㅜㅠㅡㅣㅐㅔ") == L"rsefaqtdwczxvgkijuhynbmlop");
+            CHECK(hangeul_alphabet_to_latin_alphabet(L"ㅃㅉㄸㄲㅆㅒㅖ") == L"QWERTOP");
+        }
+
+        SUBCASE("결합된 자모")
+        {
+            CHECK(hangeul_alphabet_to_latin_alphabet(L"ㄳㄵㄻㅘㅝㅞ") == L"ㄳㄵㄻㅘㅝㅞ");
+            CHECK(hangeul_alphabet_to_latin_alphabet(L"가낫똻") == L"가낫똻");
+        }
     }
 }
